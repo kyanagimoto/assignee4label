44976066aadd579635ea917d9cc47a3f9974c6dd
kyanagimoto <koichiyanagimoto@gmail.com>
2021-05-11 15:23:41 +0900

    feat: 🎸 Add assignees action with each labels

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # assignee4label
+Label action trigers to set an assignees on issues.
+
+## Usage
+
+### Create `.github/assignee4label.yml`
+
+Create a `.github/assignee4label.yml` file with a list of labels and assignees.
+
+```yml
+label1:
+- user-name
+- user-name-2
+label2:
+- user-name
+- user-name-3
+```
+
+### Create Workflow
+
+```yml
+name: "Assignees4label"
+on:
+  issues:
+    types: [labeled]
+
+jobs:
+  set-assignees:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/assignee4label@main
+      with:
+        github-token: %{{ secrets.GITHUB_TOKEN }}
+        configuration-path: '.github/assignee4label.yml'
+```
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 import * as yaml from "js-yaml";
-import { Minimatch, IMinimatch } from "minimatch";
-
-interface MatchConfig {
-  all?: string[];
-  any?: string[];
-}
-
-type StringOrMatchConfig = string | MatchConfig;
+import * as fs from "fs";
 
 async function run() {
   try {
     core.debug(`${github.context}`) 
-    const token = core.getInput("repo-token", { required: true });
+    const token = core.getInput("github-token", { required: true });
     const configPath = core.getInput("configuration-path", { required: true });
     const syncLabels = !!core.getInput("sync-labels", { required: false });
     
@@ -25,28 +18,16 @@ async function run() {
 
     const client = new github.GitHub(token);
 
-    const { data: issue } = await client.issues.get({
-        owner: github.context.issue.owner,
-        repo: github.context.repo.repo,
-        issue_number: issueNumber
-    });
+    const configurationContent: JSON = JSON.parse(JSON.stringify(yaml.load(fs.readFileSync(configPath, 'utf8'), {json: true})));
 
-    const labelGlobs: Map<string, StringOrMatchConfig[]> = await getLabelGlobs(
-      client,
-      configPath
-    );
-
-    const labels: string[] = [];
-    for (const [label, globs] of labelGlobs.entries()) {
-      core.debug(`processing ${label}`);
-      if (issue.labels.find(l => l.name === label)) {
-        labels.push(label);
+    Object.keys(configurationContent).forEach(function(key) {
+      if (github.context.payload.label.name == key) {
+        JSON.parse(JSON.stringify(configurationContent[key])).forEach(element => {
+          core.debug(`assignee name: ${element}`)
+          addAssignees(client, issueNumber, element);
+        });
       }
-    }
-
-    if (labels.length > 0) {
-      await addLabels(client, issueNumber, labels);
-    }
+    });
   } catch (error) {
     core.error(error);
     core.setFailed(error.message);
@@ -54,179 +35,37 @@ async function run() {
 }
 
 function getIssueNumber(): number | undefined {
-    const issue = github.context.payload.issue;
-    if (!issue) {
-        return undefined;
-    }
-    
-    return issue.number
+  const issue = github.context.payload.issue;
+  if (!issue) {
+      return undefined;
+  }
+  
+  return issue.number
 }
 
-async function getLabelGlobs(
+async function addAssignees(
   client: github.GitHub,
-  configurationPath: string
-): Promise<Map<string, StringOrMatchConfig[]>> {
-  const configurationContent: string = await fetchContent(
-    client,
-    configurationPath
-  );
-
-  // loads (hopefully) a `{[label:string]: string | StringOrMatchConfig[]}`, but is `any`:
-  const configObject: any = yaml.safeLoad(configurationContent);
-
-  // transform `any` => `Map<string,StringOrMatchConfig[]>` or throw if yaml is malformed:
-  return getLabelGlobMapFromObject(configObject);
-}
-
-async function fetchContent(
-  client: github.GitHub,
-  repoPath: string
-): Promise<string> {
-  const response: any = await client.repos.getContents({
+  issueNumber: number,
+  assignees: string[]
+) {
+  await client.issues.addAssignees({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    path: repoPath,
-    ref: github.context.sha
-  });
-
-  return Buffer.from(response.data.content, response.data.encoding).toString();
+    issue_number: issueNumber,
+    assignees: assignees
+  })
 }
 
-function getLabelGlobMapFromObject(
-  configObject: any
-): Map<string, StringOrMatchConfig[]> {
-  const labelGlobs: Map<string, StringOrMatchConfig[]> = new Map();
-  for (const label in configObject) {
-    if (typeof configObject[label] === "string") {
-      labelGlobs.set(label, [configObject[label]]);
-    } else if (configObject[label] instanceof Array) {
-      labelGlobs.set(label, configObject[label]);
-    } else {
-      throw Error(
-        `found unexpected type for label ${label} (should be string or array of globs)`
-      );
-    }
-  }
-
-  return labelGlobs;
-}
-
-function toMatchConfig(config: StringOrMatchConfig): MatchConfig {
-  if (typeof config === "string") {
-    return {
-      any: [config]
-    };
-  }
-
-  return config;
-}
-
-function printPattern(matcher: IMinimatch): string {
-  return (matcher.negate ? "!" : "") + matcher.pattern;
-}
-
-function checkGlobs(
-  changedFiles: string[],
-  globs: StringOrMatchConfig[]
-): boolean {
-  for (const glob of globs) {
-    core.debug(` checking pattern ${JSON.stringify(glob)}`);
-    const matchConfig = toMatchConfig(glob);
-    if (checkMatch(changedFiles, matchConfig)) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function isMatch(changedFile: string, matchers: IMinimatch[]): boolean {
-  core.debug(`    matching patterns against file ${changedFile}`);
-  for (const matcher of matchers) {
-    core.debug(`   - ${printPattern(matcher)}`);
-    if (!matcher.match(changedFile)) {
-      core.debug(`   ${printPattern(matcher)} did not match`);
-      return false;
-    }
-  }
-
-  core.debug(`   all patterns matched`);
-  return true;
-}
-
-// equivalent to "Array.some()" but expanded for debugging and clarity
-function checkAny(changedFiles: string[], globs: string[]): boolean {
-  const matchers = globs.map(g => new Minimatch(g));
-  core.debug(`  checking "any" patterns`);
-  for (const changedFile of changedFiles) {
-    if (isMatch(changedFile, matchers)) {
-      core.debug(`  "any" patterns matched against ${changedFile}`);
-      return true;
-    }
-  }
-
-  core.debug(`  "any" patterns did not match any files`);
-  return false;
-}
-
-// equivalent to "Array.every()" but expanded for debugging and clarity
-function checkAll(changedFiles: string[], globs: string[]): boolean {
-  const matchers = globs.map(g => new Minimatch(g));
-  core.debug(` checking "all" patterns`);
-  for (const changedFile of changedFiles) {
-    if (!isMatch(changedFile, matchers)) {
-      core.debug(`  "all" patterns did not match against ${changedFile}`);
-      return false;
-    }
-  }
-
-  core.debug(`  "all" patterns matched all files`);
-  return true;
-}
-
-function checkMatch(changedFiles: string[], matchConfig: MatchConfig): boolean {
-  if (matchConfig.all !== undefined) {
-    if (!checkAll(changedFiles, matchConfig.all)) {
-      return false;
-    }
-  }
-
-  if (matchConfig.any !== undefined) {
-    if (!checkAny(changedFiles, matchConfig.any)) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-async function addLabels(
+async function removeAssignees(
   client: github.GitHub,
-  prNumber: number,
-  labels: string[]
+  issueNumber: number,
+  assignees: string[]
 ) {
-  await client.issues.addLabels({
+  await client.issues.removeAssignees({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    issue_number: prNumber,
-    labels: labels
-  });
-}
-
-async function removeLabels(
-  client: github.GitHub,
-  prNumber: number,
-  labels: string[]
-) {
-  await Promise.all(
-    labels.map(label =>
-      client.issues.removeLabel({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        issue_number: prNumber,
-        name: label
-      })
-    )
-  );
+    issue_number: issueNumber
+  })
 }
 
 run();
